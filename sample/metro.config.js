a54de63f51f05026c4eb91c3e435c040ee8dcb56
bang9 <gusrn1423@naver.com>
2022-01-17 16:45:57 +0900

    feat: added ESBuild to sample metro bundler minifier

--- sample/metro.config.js
+++ sample/metro.config.js
@@ -5,14 +5,17 @@
  * @format
  */
 
-const exclusionList = require("metro-config/src/defaults/exclusionList");
-const { getMetroTools, getMetroAndroidAssetsResolutionFix } = require("react-native-monorepo-tools");
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const {
+  getMetroTools,
+  getMetroAndroidAssetsResolutionFix,
+} = require('react-native-monorepo-tools');
 const monorepoMetroTools = getMetroTools();
 const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix();
 
 module.exports = {
   transformer: {
-    // Apply the Android assets resolution fix to the public path...
+    minifierPath: 'metro-minify-esbuild',
     publicPath: androidAssetsResolutionFix.publicPath,
     getTransformOptions: async () => ({
       transform: {
@@ -22,16 +25,12 @@ module.exports = {
     }),
   },
   server: {
-    // ...and to the server middleware.
     enhanceMiddleware: middleware => {
       return androidAssetsResolutionFix.applyMiddleware(middleware);
     },
   },
-  // Add additional Yarn workspace package roots to the module map.
-  // This allows importing importing from all the project's packages.
   watchFolders: monorepoMetroTools.watchFolders,
   resolver: {
-    // Ensure we resolve nohoist libraries from this directory.
     blockList: exclusionList(monorepoMetroTools.blockList),
     extraNodeModules: monorepoMetroTools.extraNodeModules,
   },
