e6eca269df4641faf5ebb8a0f91be2e0aa000324
bang9 <gusrn1423@naver.com>
2022-02-08 05:49:57 +0900

    fix: remove default storybook components

--- sample/storybook/stories/GroupChannelPreview.stories.tsx
+++ sample/storybook/stories/GroupChannelPreview.stories.tsx
@@ -7,21 +7,19 @@ import { Alert } from 'react-native';
 import { GroupChannelPreview } from '@sendbird/uikit-react-native';
 import SBIconAssets from '@sendbird/uikit-react-native/src/assets/icon';
 
-storiesOf('GroupChannelPreview', module)
-  // .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('GroupChannelPreview', () => (
-    <>
-      <GroupChannelPreview
-        coverUrl={text('Cover uri', 'https://static.sendbird.com/sample/cover/cover_15.jpg')}
-        onPress={() => Alert.alert('pressed')}
-        title={text('Title', 'Title')}
-        titleCaption={text('Title Caption', 'Caption')}
-        body={text('Body', 'Body')}
-        bodyIcon={select('Body Icon', Object.keys(SBIconAssets) as any, 'file-document')}
-        memberCount={number('Member count', 10)}
-        badgeCount={number('Badge count', 5)}
-        frozen={boolean('Frozen', true)}
-        muted={boolean('Muted', true)}
-      />
-    </>
-  ));
+storiesOf('GroupChannelPreview', module).add('GroupChannelPreview', () => (
+  <>
+    <GroupChannelPreview
+      coverUrl={text('Cover uri', 'https://static.sendbird.com/sample/cover/cover_15.jpg')}
+      onPress={() => Alert.alert('pressed')}
+      title={text('Title', 'Title')}
+      titleCaption={text('Title Caption', 'Caption')}
+      body={text('Body', 'Body')}
+      bodyIcon={select('Body Icon', Object.keys(SBIconAssets) as any, 'file-document')}
+      memberCount={number('Member count', 10)}
+      badgeCount={number('Badge count', 5)}
+      frozen={boolean('Frozen', true)}
+      muted={boolean('Muted', true)}
+    />
+  </>
+));
