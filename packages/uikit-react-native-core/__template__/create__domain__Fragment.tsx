bcb171096107bb9abd4cc0735ce126167db66718
bang9 <gusrn1423@naver.com>
2022-03-16 00:09:13 +0900

    chore(core): update template scripts

--- packages/uikit-react-native-core/__template__/create__domain__Fragment.tsx
+++ packages/uikit-react-native-core/__template__/create__domain__Fragment.tsx
@@ -1,11 +1,9 @@
 // @ts-nocheck - !!REMOVE
-//TODO: move to @sendbird/uikit-react-native/fragments
 import React from 'react';
 
 import type { __domain__Fragment, __domain__Module } from '@sendbird/uikit-react-native-core';
+import { create__domain__Module } from '@sendbird/uikit-react-native-core';
 import { EmptyFunction } from '@sendbird/uikit-utils';
-
-import create__domain__Module from '../module/create__domain__Module';
 
 const create__domain__Fragment = (initModule?: __domain__Module): __domain__Fragment => {
   const __domain__Module = create__domain__Module(initModule);
