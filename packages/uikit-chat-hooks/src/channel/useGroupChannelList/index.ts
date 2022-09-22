de5fc4c08257f1ec41e11ff42f9429102e07490c
bang9 <gusrn1423@naver.com>
2022-09-22 17:39:02 +0900

    chore(chat-hooks): added warning for queryCreator

--- packages/uikit-chat-hooks/src/channel/useGroupChannelList/index.ts
+++ packages/uikit-chat-hooks/src/channel/useGroupChannelList/index.ts
@@ -1,9 +1,12 @@
+import { Logger } from '@sendbird/uikit-utils';
+
 import type { UseGroupChannelList } from '../../types';
 import { useGroupChannelListWithCollection } from './useGroupChannelListWithCollection';
 import { useGroupChannelListWithQuery } from './useGroupChannelListWithQuery';
 
 export const useGroupChannelList: UseGroupChannelList = (sdk, userId, options) => {
   if (sdk.isCacheEnabled || options?.enableCollectionWithoutLocalCache) {
+    if (options?.queryCreator) Logger.warn('`queryCreator` is ignored, please use `collectionCreator` instead.');
     return useGroupChannelListWithCollection(sdk, userId, options);
   } else {
     return useGroupChannelListWithQuery(sdk, userId, options);
