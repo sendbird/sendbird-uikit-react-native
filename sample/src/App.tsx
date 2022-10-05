1f664269095f4f7f4f8cb796f323412d6c33857e
bang9 <gusrn1423@naver.com>
2022-10-05 17:05:58 +0900

    chore(sample): update badge count when app state changed

--- sample/src/App.tsx
+++ sample/src/App.tsx
@@ -2,6 +2,7 @@ import Notifee from '@notifee/react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import React, { useEffect } from 'react';
+import { AppState } from 'react-native';
 
 import { SendbirdUIKitContainer, useSendbirdChat } from '@sendbird/uikit-react-native';
 import { DarkUIKitTheme, LightUIKitTheme } from '@sendbird/uikit-react-native-foundation';
@@ -68,16 +69,22 @@ const App = () => {
 };
 
 const Navigations = () => {
-  const { currentUser } = useSendbirdChat();
+  const { sdk, currentUser } = useSendbirdChat();
   const { scheme } = useAppearance();
   const isLightTheme = scheme === 'light';
 
   useEffect(() => {
-    Notifee.setBadgeCount(0);
     const unsubscribes = [onForegroundAndroid(), onForegroundIOS()];
     return () => {
       unsubscribes.forEach((fn) => fn());
     };
+  }, []);
+
+  useEffect(() => {
+    AppState.addEventListener('change', async () => {
+      const count = await sdk.groupChannel.getTotalUnreadMessageCount();
+      Notifee.setBadgeCount(count);
+    });
   }, []);
 
   return (
