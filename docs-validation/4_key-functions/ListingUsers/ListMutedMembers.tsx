877d7d8928b8cfebbb64320756e940e959991e9a
bang9 <gusrn1423@naver.com>
2022-11-10 17:19:42 +0900

    docs: added list muted members

---
+++ docs-validation/4_key-functions/ListingUsers/ListMutedMembers.tsx
@@ -0,0 +1,38 @@
+const MyHeader = () => null;
+
+/**
+ *
+ * {@link }
+ * */
+import React from 'react';
+
+import { useGroupChannel } from '@sendbird/uikit-chat-hooks';
+import { createGroupChannelMutedMembersFragment, useSendbirdChat } from '@sendbird/uikit-react-native';
+
+const GroupChannelMutedMembersFragment = createGroupChannelMutedMembersFragment();
+const GroupChannelMutedMembersScreen = (params: { channelUrl: string }) => {
+    const { sdk } = useSendbirdChat();
+    const { channel } = useGroupChannel(sdk, params.channelUrl);
+    if (!channel) return null;
+
+    const navigateToBack = () => {};
+
+    return <GroupChannelMutedMembersFragment channel={channel} onPressHeaderLeft={navigateToBack} />;
+};
+
+/**
+ *
+ * {@link }
+ * */
+const GroupChannelMutedMembersFragment2 = createGroupChannelMutedMembersFragment({
+    Header: () => <MyHeader />, // Use custom header
+});
+const GroupChannelMutedMembersScreen2 = (params: { channelUrl: string }) => {
+    const { sdk } = useSendbirdChat();
+    const { channel } = useGroupChannel(sdk, params.channelUrl);
+    if (!channel) return null;
+
+    const navigateToBack = () => {};
+
+    return <GroupChannelMutedMembersFragment channel={channel} onPressHeaderLeft={navigateToBack} />;
+};
