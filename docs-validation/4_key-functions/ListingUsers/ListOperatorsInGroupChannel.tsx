a4afbf75e80432fe5cd7ada79b1edb1f33db93d0
bang9 <gusrn1423@naver.com>
2022-11-11 14:56:54 +0900

    docs: added list operators in group channel

---
+++ docs-validation/4_key-functions/ListingUsers/ListOperatorsInGroupChannel.tsx
@@ -0,0 +1,52 @@
+const MyHeader = () => null;
+
+/**
+ *
+ * {@link }
+ * */
+import React from 'react';
+
+import { useGroupChannel } from '@sendbird/uikit-chat-hooks';
+import { createGroupChannelOperatorsFragment, useSendbirdChat } from '@sendbird/uikit-react-native';
+
+const GroupChannelOperatorsFragment = createGroupChannelOperatorsFragment();
+const GroupChannelOperatorsScreen = (params: { channelUrl: string }) => {
+  const { sdk } = useSendbirdChat();
+  const { channel } = useGroupChannel(sdk, params.channelUrl);
+  if (!channel) return null;
+
+  const navigateToBack = () => {};
+  const navigateToGroupChannelRegisterOperator = () => {};
+
+  return (
+    <GroupChannelOperatorsFragment
+      channel={channel}
+      onPressHeaderLeft={navigateToBack}
+      onPressHeaderRight={navigateToGroupChannelRegisterOperator}
+    />
+  );
+};
+
+/**
+ *
+ * {@link }
+ * */
+const GroupChannelOperatorsFragment2 = createGroupChannelOperatorsFragment({
+  Header: () => <MyHeader />, // Use custom header
+});
+const GroupChannelOperatorsScreen2 = (params: { channelUrl: string }) => {
+  const { sdk } = useSendbirdChat();
+  const { channel } = useGroupChannel(sdk, params.channelUrl);
+  if (!channel) return null;
+
+  const navigateToBack = () => {};
+  const navigateToGroupChannelRegisterOperator = () => {};
+
+  return (
+    <GroupChannelOperatorsFragment
+      channel={channel}
+      onPressHeaderLeft={navigateToBack}
+      onPressHeaderRight={navigateToGroupChannelRegisterOperator}
+    />
+  );
+};
