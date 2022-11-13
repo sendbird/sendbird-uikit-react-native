7dde22261dca51e19c2d9475141be18ec74aa22b
bang9 <gusrn1423@naver.com>
2022-11-13 16:46:42 +0900

    docs: added register member as operators

---
+++ docs-validation/4_key-functions/RegisteringAsOperator/RegisterMemberAsOperators.tsx
@@ -0,0 +1,52 @@
+const MyHeader = () => null;
+
+/**
+ *
+ * {@link https://sendbird.com/docs/uikit/v3/react-native/key-functions/registering-as-operator-register-member-as-operators}
+ * */
+import React from 'react';
+
+import { useGroupChannel } from '@sendbird/uikit-chat-hooks';
+import { createGroupChannelRegisterOperatorFragment, useSendbirdChat } from '@sendbird/uikit-react-native';
+
+const GroupChannelRegisterOperatorFragment = createGroupChannelRegisterOperatorFragment();
+const GroupChannelRegisterOperatorScreen = (params: { channelUrl: string }) => {
+  const { sdk } = useSendbirdChat();
+  const { channel } = useGroupChannel(sdk, params.channelUrl);
+  if (!channel) return null;
+
+  const navigateToBack = () => {};
+  const navigateToGroupChannelOperatorsScreen = () => {};
+
+  return (
+    <GroupChannelRegisterOperatorFragment
+      channel={channel}
+      onPressHeaderLeft={navigateToBack}
+      onPressHeaderRight={navigateToGroupChannelOperatorsScreen}
+    />
+  );
+};
+
+/**
+ *
+ * {@link https://sendbird.com/docs/uikit/v3/react-native/key-functions/registering-as-operator-register-member-as-operators}
+ * */
+const GroupChannelRegisterOperatorFragment2 = createGroupChannelRegisterOperatorFragment({
+    Header: () => <MyHeader />, // Use custom header
+});
+const GroupChannelRegisterOperatorScreen2 = (params: { channelUrl: string }) => {
+    const { sdk } = useSendbirdChat();
+    const { channel } = useGroupChannel(sdk, params.channelUrl);
+    if (!channel) return null;
+
+    const navigateToBack = () => {};
+    const navigateToGroupChannelOperatorsScreen = () => {};
+
+    return (
+        <GroupChannelRegisterOperatorFragment
+            channel={channel}
+            onPressHeaderLeft={navigateToBack}
+            onPressHeaderRight={navigateToGroupChannelOperatorsScreen}
+        />
+    );
+};
