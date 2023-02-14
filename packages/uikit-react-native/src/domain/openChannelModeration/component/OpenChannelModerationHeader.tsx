0937606c672e0575299ce082bb591835eef938eb
bang9 <gusrn1423@naver.com>
2023-02-14 10:24:16 +0900

    chore: added open channel moderation header module

--- packages/uikit-react-native/src/domain/openChannelModeration/component/OpenChannelModerationHeader.tsx
+++ packages/uikit-react-native/src/domain/openChannelModeration/component/OpenChannelModerationHeader.tsx
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react';
 
-import { useHeaderStyle } from '@sendbird/uikit-react-native-foundation';
+import { Icon, useHeaderStyle } from '@sendbird/uikit-react-native-foundation';
 
 import { OpenChannelModerationContexts } from '../module/moduleContext';
 import type { OpenChannelModerationProps } from '../types';
 
-const OpenChannelModerationHeader = (_: OpenChannelModerationProps['Header']) => {
+const OpenChannelModerationHeader = ({ onPressHeaderLeft }: OpenChannelModerationProps['Header']) => {
   const { headerTitle } = useContext(OpenChannelModerationContexts.Fragment);
   const { HeaderComponent } = useHeaderStyle();
-  return <HeaderComponent title={headerTitle} />;
+
+  return <HeaderComponent title={headerTitle} left={<Icon icon={'arrow-left'} />} onPressLeft={onPressHeaderLeft} />;
 };
 
 export default OpenChannelModerationHeader;
