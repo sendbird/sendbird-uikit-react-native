db78cc0f95da8dbc0a66cc079191ac7e05b903bd
bang9 <gusrn1423@naver.com>
2023-03-04 14:54:26 +0900

    test: added environments to testing hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   'env': {
     'es2021': true,
+    'jest': true,
   },
   'extends': ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   'parser': '@typescript-eslint/parser',
