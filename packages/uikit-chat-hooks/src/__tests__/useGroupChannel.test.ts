f09db5a7d12710073e0ef52d42e4e77056bc4631
bang9 <gusrn1423@naver.com>
2023-03-04 14:56:52 +0900

    test: added test case for useGroupChannel

---
+++ packages/uikit-chat-hooks/src/__tests__/useGroupChannel.test.ts
@@ -0,0 +1,40 @@
+import { renderHook, waitFor } from '@testing-library/react-native';
+
+import { useGroupChannel } from '../channel/useGroupChannel';
+import { createMockSendbird } from './__mocks__/createMockSendbirdSDK';
+
+describe('useGroupChannel', () => {
+  it('should fetch group channel and return it', async () => {
+    const channelUrl = 'test_channel_url';
+    const sdk = createMockSendbird('success');
+    const { result } = renderHook(() => useGroupChannel(sdk, channelUrl));
+
+    expect(result.current.loading).toBe(true);
+    expect(result.current.channel).toBeUndefined();
+    expect(result.current.error).toBeUndefined();
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.channel).toBeDefined();
+      expect(result.current.error).toBeUndefined();
+      expect(sdk.groupChannel.getChannel).toHaveBeenCalledWith(channelUrl);
+    });
+  });
+
+  it('should set error state if fetching group channel fails', async () => {
+    const channelUrl = 'test_channel_url';
+    const sdk = createMockSendbird('failure');
+    const { result } = renderHook(() => useGroupChannel(sdk, channelUrl));
+
+    expect(result.current.loading).toBe(true);
+    expect(result.current.channel).toBeUndefined();
+    expect(result.current.error).toBeUndefined();
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.channel).toBeUndefined();
+      expect(result.current.error).toBeDefined();
+      expect(sdk.groupChannel.getChannel).toHaveBeenCalledWith(channelUrl);
+    });
+  });
+});
