b6275aa36e068d293b8e9e41df1582d349175247
bang9 <gusrn1423@naver.com>
2023-03-14 15:05:34 +0900

    test: expand test coverage for useChannelHandler test

--- packages/uikit-chat-hooks/src/__tests__/handler/useChannelHandler.test.ts
+++ packages/uikit-chat-hooks/src/__tests__/handler/useChannelHandler.test.ts
@@ -4,16 +4,46 @@ import { useChannelHandler } from '../../handler/useChannelHandler';
 import { createMockSendbird } from '../__mocks__/createMockSendbirdSDK';
 
 describe('useChannelHandler', () => {
-  it('should add and remove the channel handler when mounted and unmounted', () => {
+  it('should add and remove the group channel handler when mounted and unmounted', () => {
     const sdk = createMockSendbird();
     const handlerId = 'test-handler-id';
     const handler = { onMessageReceived: jest.fn() };
 
     const { unmount } = renderHook(() => useChannelHandler(sdk, handlerId, handler, 'group'));
+
     expect(sdk.groupChannel.addGroupChannelHandler).toHaveBeenCalledWith(handlerId, expect.any(Object));
+    expect(sdk.openChannel.addOpenChannelHandler).not.toHaveBeenCalled();
 
     unmount();
+
     expect(sdk.groupChannel.removeGroupChannelHandler).toHaveBeenCalledWith(handlerId);
+    expect(sdk.openChannel.removeOpenChannelHandler).not.toHaveBeenCalled();
+  });
+
+  it('should add and remove the open channel handler when mounted and unmounted', () => {
+    const sdk = createMockSendbird();
+    const handlerId = 'test-handler-id';
+    const handler = { onMessageReceived: jest.fn() };
+
+    const { unmount } = renderHook(() => useChannelHandler(sdk, handlerId, handler, 'open'));
+
+    expect(sdk.openChannel.addOpenChannelHandler).toHaveBeenCalledWith(handlerId, expect.any(Object));
+    expect(sdk.groupChannel.addGroupChannelHandler).not.toHaveBeenCalled();
+
+    unmount();
+
+    expect(sdk.openChannel.removeOpenChannelHandler).toHaveBeenCalledWith(handlerId);
+    expect(sdk.groupChannel.removeGroupChannelHandler).not.toHaveBeenCalled();
+  });
+
+  it('should use group type as default when type is not provided to useChannelHandler', () => {
+    const sdk = createMockSendbird();
+    const handlerId = 'test-handler-id';
+    const handler = { onMessageReceived: jest.fn() };
+
+    renderHook(() => useChannelHandler(sdk, handlerId, handler));
+
+    expect(sdk.groupChannel.addGroupChannelHandler).toHaveBeenCalledWith(handlerId, expect.any(Object));
   });
 
   it('should channel handler triggered when event received', () => {
@@ -27,7 +57,7 @@ describe('useChannelHandler', () => {
     expect(handler.onMessageReceived).toHaveBeenCalledTimes(1);
     sdk.__emit('channel', 'group_onMessageReceived');
     expect(handler.onMessageReceived).toHaveBeenCalledTimes(2);
-    sdk.__emit('channel', 'group_onMessageUpdated');
+    sdk.__emit('channel', 'open_onMessageReceived');
     expect(handler.onMessageReceived).toHaveBeenCalledTimes(2);
   });
 });
