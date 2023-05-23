fee65a9978a04d682e877ebe40a426619f1b82ac
bang9 <gusrn1423@naver.com>
2023-05-23 17:57:55 +0900

    chore: implement unknown message

--- packages/uikit-react-native-foundation/src/ui/GroupChannelMessage/Message.unknown.tsx
+++ packages/uikit-react-native-foundation/src/ui/GroupChannelMessage/Message.unknown.tsx
@@ -2,30 +2,44 @@ import React from 'react';
 
 import type { SendbirdMessage } from '@sendbird/uikit-utils';
 
+import Box from '../../components/Box';
 import PressBox from '../../components/PressBox';
 import Text from '../../components/Text';
+import createStyleSheet from '../../styles/createStyleSheet';
 import useUIKitTheme from '../../theme/useUIKitTheme';
 import MessageContainer from './MessageContainer';
 import type { GroupChannelMessageProps } from './index';
 
 const UnknownMessage = (props: GroupChannelMessageProps<SendbirdMessage>) => {
   const { colors } = useUIKitTheme();
-  const { onPress, onLongPress, ...rest } = props;
-  const color = colors.ui.openChannelMessage.default;
+  const color = colors.ui.groupChannelMessage[props.variant ?? 'incoming'];
+  const titleColor = props.variant === 'incoming' ? colors.onBackground01 : colors.onBackgroundReverse01;
+  const descColor = props.variant === 'incoming' ? colors.onBackground02 : colors.onBackgroundReverse02;
+
   return (
-    <PressBox onPress={onPress} onLongPress={onLongPress}>
-      {({ pressed }) => (
-        <MessageContainer pressed={pressed} {...rest}>
-          <Text body3 color={color.enabled.textMsg}>
-            {props.strings?.unknownTitle ?? '(Unknown message type)'}
-          </Text>
-          <Text body3 color={color.enabled.textMsgPostfix}>
-            {props.strings?.unknownDescription ?? 'Cannot read this message.'}
-          </Text>
-        </MessageContainer>
-      )}
-    </PressBox>
+    <MessageContainer {...props}>
+      <PressBox onPress={props.onPress} onLongPress={props.onLongPress}>
+        {({ pressed }) => (
+          <Box style={styles.bubble} backgroundColor={pressed ? color.pressed.background : color.enabled.background}>
+            <Text body3 color={titleColor}>
+              {props.strings?.unknownTitle ?? '(Unknown message type)'}
+            </Text>
+            <Text body3 color={descColor}>
+              {props.strings?.unknownDescription ?? 'Cannot read this message.'}
+            </Text>
+          </Box>
+        )}
+      </PressBox>
+    </MessageContainer>
   );
 };
+
+const styles = createStyleSheet({
+  bubble: {
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 16,
+  },
+});
 
 export default UnknownMessage;
