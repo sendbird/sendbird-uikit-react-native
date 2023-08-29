18f23ff5ec5f14afb5cfe408f0a6f95cbf2169ea
bang9 <gusrn1423@naver.com>
2023-08-29 13:03:30 +0900

    chore: add state listener to player service

--- packages/uikit-react-native/src/platform/types.ts
+++ packages/uikit-react-native/src/platform/types.ts
@@ -107,6 +107,7 @@ export interface MediaServiceInterface {
 
 // ---------- PlayerService ---------- //
 export interface PlayerServiceInterface {
+  uri?: string;
   state: 'idle' | 'preparing' | 'playing' | 'paused' | 'stopped';
 
   /**
@@ -120,6 +121,11 @@ export interface PlayerServiceInterface {
   addPlaybackListener(
     callback: (params: { currentTime: number; duration: number; stopped: boolean }) => void,
   ): Unsubscribe;
+
+  /**
+   * Add a state listener.
+   * */
+  addStateListener(callback: (state: PlayerServiceInterface['state']) => void): Unsubscribe;
 
   /**
    * State transition:
