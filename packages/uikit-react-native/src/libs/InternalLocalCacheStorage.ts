ffbb8fcc0434f914355cb28a9d522c6e276a6f7c
bang9 <gusrn1423@naver.com>
2024-06-21 13:00:22 +0900

    feat: support mmkv storage and deprecate a async storage

--- packages/uikit-react-native/src/libs/InternalLocalCacheStorage.ts
+++ packages/uikit-react-native/src/libs/InternalLocalCacheStorage.ts
@@ -1,47 +1,96 @@
 import { ASYNC_NOOP } from '@sendbird/uikit-utils';
 
-import type { KeyValuePairGet, KeyValuePairSet, LocalCacheStorage } from '../types';
+import type {
+  AsyncLocalCacheStorage,
+  KeyValuePairGet,
+  KeyValuePairSet,
+  LocalCacheStorage,
+  MMKVLocalCacheStorage,
+} from '../types';
 
-export default class InternalLocalCacheStorage implements LocalCacheStorage {
-  constructor(private storage: LocalCacheStorage) {}
+export default class InternalLocalCacheStorage implements AsyncLocalCacheStorage {
+  private readonly _mmkv?: MMKVLocalCacheStorage;
+  private readonly _async?: AsyncLocalCacheStorage;
 
-  getAllKeys() {
-    return this.storage.getAllKeys();
+  constructor(storage: LocalCacheStorage) {
+    if ('getString' in storage) {
+      this._mmkv = storage;
+    } else {
+      this._async = storage;
+    }
   }
 
-  getItem(key: string) {
-    return this.storage.getItem(key);
+  async getAllKeys() {
+    if (this._mmkv) {
+      return this._mmkv.getAllKeys();
+    } else if (this._async) {
+      return this._async.getAllKeys();
+    } else {
+      return [];
+    }
   }
 
-  removeItem(key: string) {
-    return this.storage.removeItem(key);
+  async getItem(key: string) {
+    if (this._mmkv) {
+      return this._mmkv.getString(key) ?? null;
+    } else if (this._async) {
+      return this._async.getItem(key);
+    } else {
+      return null;
+    }
   }
 
-  setItem(key: string, value: string) {
-    return this.storage.setItem(key, value);
+  async removeItem(key: string) {
+    if (this._mmkv) {
+      this._mmkv.delete(key);
+    } else if (this._async) {
+      return this._async.removeItem(key);
+    }
+  }
+
+  async setItem(key: string, value: string) {
+    if (this._mmkv) {
+      this._mmkv.set(key, value);
+    } else if (this._async) {
+      return this._async.setItem(key, value);
+    }
   }
 
   async multiGet(keys: string[]) {
-    if (this.storage.multiGet) {
-      return this.storage.multiGet(keys);
-    } else {
+    if (this._mmkv) {
       return Promise.all(keys.map(async (key) => [key, await this.getItem(key)] as KeyValuePairGet));
+    } else if (this._async) {
+      if (this._async?.multiGet) {
+        return this._async.multiGet(keys);
+      } else {
+        return Promise.all(keys.map(async (key) => [key, await this.getItem(key)] as KeyValuePairGet));
+      }
+    } else {
+      return [];
     }
   }
 
   async multiRemove(keys: string[]) {
-    if (this.storage.multiRemove) {
-      await this.storage.multiRemove(keys);
-    } else {
+    if (this._mmkv) {
       await Promise.all(keys.map(async (key) => this.removeItem(key)));
+    } else if (this._async) {
+      if (this._async?.multiRemove) {
+        await this._async.multiRemove(keys);
+      } else {
+        await Promise.all(keys.map(async (key) => this.removeItem(key)));
+      }
     }
   }
 
   async multiSet(keyValuePairs: Array<KeyValuePairSet>) {
-    if (this.storage.multiSet) {
-      await this.storage.multiSet(keyValuePairs);
-    } else {
-      await Promise.all(keyValuePairs.map(([key, value]) => this.storage.setItem(key, value)));
+    if (this._mmkv) {
+      await Promise.all(keyValuePairs.map(([key, value]) => this.setItem(key, value)));
+    } else if (this._async) {
+      if (this._async?.multiSet) {
+        await this._async.multiSet(keyValuePairs);
+      } else {
+        await Promise.all(keyValuePairs.map(([key, value]) => this.setItem(key, value)));
+      }
     }
   }
 
