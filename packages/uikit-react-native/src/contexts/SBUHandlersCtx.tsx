47fff478fa3ec7954cb83c1d10c164880d824dcc
bang9 <gusrn1423@naver.com>
2024-09-20 17:09:48 +0900

    chore: add jsdocs

--- packages/uikit-react-native/src/contexts/SBUHandlersCtx.tsx
+++ packages/uikit-react-native/src/contexts/SBUHandlersCtx.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export interface SBUHandlers {
+  /**
+   * Callback function to handle opening a URL.
+   * This is triggered when a URL needs to be opened.
+   */
   onOpenURL: (url: string) => void;
+
+  /**
+   * Callback function to handle opening a file URL.
+   * This is triggered when a file URL needs to be opened.
+   *
+   * Note that this function is also called redundantly
+   * when `onPressMediaMessage` handler is triggered by clicking on media messages containing images, videos, or audio.
+   */
   onOpenFileURL: (url: string) => void;
 }
 
