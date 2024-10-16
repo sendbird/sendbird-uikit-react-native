ed9b5b5dfec38dd25f213d1251306cb3a29110e3
bang9 <gusrn1423@naver.com>
2024-10-16 18:32:36 +0900

    feat: apply rtl support to open channel fragments

--- packages/uikit-react-native-foundation/src/ui/OpenChannelMessage/Message.user.tsx
+++ packages/uikit-react-native-foundation/src/ui/OpenChannelMessage/Message.user.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import type { SendbirdUserMessage } from '@sendbird/uikit-utils';
 import { urlRegexStrict } from '@sendbird/uikit-utils';
 
+import Box from '../../components/Box';
 import PressBox from '../../components/PressBox';
 import RegexText from '../../components/RegexText';
 import Text from '../../components/Text';
@@ -20,38 +21,54 @@ const UserMessage = (props: OpenChannelMessageProps<SendbirdUserMessage, Props>)
     <PressBox onPress={onPress} onLongPress={onLongPress}>
       {({ pressed }) => (
         <MessageContainer pressed={pressed} {...rest}>
-          <Text body3 color={color.enabled.textMsg}>
-            <RegexText
+          <Box alignItems={'flex-start'}>
+            <Text
               body3
               color={color.enabled.textMsg}
-              patterns={[
-                {
-                  regex: urlRegexStrict,
-                  replacer({ match, parentProps, keyPrefix, index }) {
-                    return (
-                      <Text
-                        {...parentProps}
-                        key={`${keyPrefix}-${index}`}
-                        onPress={() => onPressURL?.(match)}
-                        onLongPress={onLongPress}
-                        color={colors.primary}
-                        style={parentProps?.style}
-                      >
-                        {match}
-                      </Text>
-                    );
-                  },
-                },
-              ]}
+              suppressHighlighting
+              supportRTLAlign
+              originalText={props.message.message}
             >
-              {props.message.message}
-            </RegexText>
-            {Boolean(props.message.updatedAt) && (
-              <Text body3 color={color.enabled.textMsgPostfix}>
-                {props.strings?.edited ?? ' (edited)'}
-              </Text>
-            )}
-          </Text>
+              <RegexText
+                body3
+                suppressHighlighting
+                supportRTLAlign
+                originalText={props.message.message}
+                color={color.enabled.textMsg}
+                patterns={[
+                  {
+                    regex: urlRegexStrict,
+                    replacer({ match, parentProps, keyPrefix, index }) {
+                      return (
+                        <Text
+                          {...parentProps}
+                          key={`${keyPrefix}-${index}`}
+                          onPress={() => onPressURL?.(match)}
+                          onLongPress={onLongPress}
+                          color={colors.primary}
+                          style={parentProps?.style}
+                        >
+                          {match}
+                        </Text>
+                      );
+                    },
+                  },
+                ]}
+              >
+                {props.message.message}
+              </RegexText>
+              {Boolean(props.message.updatedAt) && (
+                <Text
+                  body3
+                  color={color.enabled.textMsgPostfix}
+                  supportRTLAlign
+                  originalText={props.strings?.edited ?? ' (edited)'}
+                >
+                  {props.strings?.edited ?? ' (edited)'}
+                </Text>
+              )}
+            </Text>
+          </Box>
         </MessageContainer>
       )}
     </PressBox>
