b65b30b2ca63cd86ba4dcf06a922dccc61f4f06a
OnestarLee <onestar.lee@sendbird.com>
2025-06-30 10:57:52 +0900

    chore: add unread message string and icon resources for docs

---
+++ docs-validation/2_features/UnreadMessage.ts
@@ -0,0 +1,33 @@
+import type { StringSet } from '@sendbird/uikit-react-native';
+
+/**
+ * String resource
+ * {@link https://sendbird.com/docs/chat/uikit/v3/react-native/features/reactions}
+ * */
+function _stringResource(str: StringSet) {
+  str.GROUP_CHANNEL.LIST_FLOATING_UNREAD_MSG;
+  str.LABELS.CHANNEL_MESSAGE_MARK_AS_UNREAD;
+}
+/** ------------------ **/
+// interface StringSet {
+//   GROUP_CHANNEL: {
+//     LIST_FLOATING_UNREAD_MSG: (unreadMessageCount: number) => string;
+//   };
+// }
+//
+// interface StringSet {
+//   LABELS: {
+//     CHANNEL_MESSAGE_MARK_AS_UNREAD: string;
+//   };
+// }
+
+/** ------------------ **/
+
+/**
+ * Icon resource
+ * {@link https://sendbird.com/docs/chat/uikit/v3/react-native/features/reactions#mark-as-unread-icon-resource}
+ * */
+import { Icon } from '@sendbird/uikit-react-native-foundation';
+
+Icon.Assets['mark-as-unread'] = require('your_icons/icon-mark-as-unread.png');
+/** ------------------ **/
